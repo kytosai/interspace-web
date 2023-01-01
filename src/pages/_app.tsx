86aa4b7aa6282fa37eccc6f515b84dc3b2676a0e
kytosai <kytosai@gmail.com>
2023-01-01 11:03:33 +0700

    add viewport scale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import 'normalize.css';
 import '@/assets/styles/global.scss';
 import { AppPropsExtended } from '@/types/common';
 import EmptyLayout from '@/components/layouts/EmptyLayout';
+import Head from 'next/head';
 
 export default function App(props: AppPropsExtended) {
   const { Component, pageProps } = props;
@@ -9,6 +10,13 @@ export default function App(props: AppPropsExtended) {
 
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="initial-scale=1, width=device-width, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
